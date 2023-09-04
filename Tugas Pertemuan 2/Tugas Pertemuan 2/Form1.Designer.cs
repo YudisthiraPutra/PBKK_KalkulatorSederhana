8329f840ba02a958ee5b61b14e74bd30d499ebb4
YudisthiraPutra <114007340+YudisthiraPutra@users.noreply.github.com>
2023-09-04 23:17:35 +0700

    First Commit

---
+++ Tugas Pertemuan 2/Tugas Pertemuan 2/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Tugas_Pertemuan_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Franklin Gothic Medium", 28.125F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox1.Location = new Point(315, 80);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(395, 93);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(55, 707);
+            button1.Name = "button1";
+            button1.Size = new Size(203, 202);
+            button1.TabIndex = 1;
+            button1.Text = "x";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(277, 489);
+            button2.Name = "button2";
+            button2.Size = new Size(203, 202);
+            button2.TabIndex = 2;
+            button2.Text = "/";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.Location = new Point(55, 489);
+            button3.Name = "button3";
+            button3.Size = new Size(203, 202);
+            button3.TabIndex = 3;
+            button3.Text = "+";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button4.Location = new Point(277, 707);
+            button4.Name = "button4";
+            button4.Size = new Size(203, 202);
+            button4.TabIndex = 4;
+            button4.Text = "-";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button5.Location = new Point(507, 489);
+            button5.Name = "button5";
+            button5.Size = new Size(203, 420);
+            button5.TabIndex = 5;
+            button5.Text = "Reset";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Franklin Gothic Medium", 28.125F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox2.Location = new Point(315, 194);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(395, 93);
+            textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Franklin Gothic Medium", 28.125F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox3.Location = new Point(315, 311);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(395, 93);
+            textBox3.TabIndex = 7;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.ControlLightLight;
+            label1.Location = new Point(38, 110);
+            label1.Name = "label1";
+            label1.Size = new Size(217, 45);
+            label1.TabIndex = 8;
+            label1.Text = "First Number";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = SystemColors.ControlLightLight;
+            label2.Location = new Point(38, 216);
+            label2.Name = "label2";
+            label2.Size = new Size(261, 45);
+            label2.TabIndex = 9;
+            label2.Text = "Second Number";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.ForeColor = SystemColors.ControlLightLight;
+            label3.Location = new Point(38, 336);
+            label3.Name = "label3";
+            label3.Size = new Size(111, 45);
+            label3.TabIndex = 10;
+            label3.Text = "Result";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 10.875F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.ForeColor = SystemColors.ControlLightLight;
+            label4.Location = new Point(322, 1166);
+            label4.Name = "label4";
+            label4.Size = new Size(0, 40);
+            label4.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.SeaGreen;
+            ClientSize = new Size(768, 959);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+    }
+}
